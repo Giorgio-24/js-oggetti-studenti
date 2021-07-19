606f603a00f043dd0f668b93b6923440dbb726c9
Giorgio Andrei <cipuandrei@libero.it>
2021-07-19 19:18:48 +0200

    students-array-created

---
+++ script.js
@@ -0,0 +1,27 @@
+const students = [
+    student1, {
+        firstName: "Jonny",
+        lastName: "Bravo",
+        age: 17
+    },
+    student2, {
+        firstName: "Leonard",
+        lastName: "Coler",
+        age: 18
+    },
+    student3, {
+        firstName: "Mike",
+        lastName: "Enner",
+        age: 18
+    },
+    student4, {
+        firstName: "Bruce",
+        lastName: "Lensen",
+        age: 17
+    },
+    student5, {
+        firstName: "Edward",
+        lastName: "Robinson",
+        age: 17
+    }
+];
